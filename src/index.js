97d3660025f46c0c19bdf0509d52e8c574a604e7
Vipin sharma <vicky.sharma8127344@gmail.com>
2022-04-10 20:34:18 +0530

    - Install sass, style-loader, css-loader, sass-loader - Install html-webpack-plugin - Add html template file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '../src/assets/style.scss';
 import { randomQuotes } from './quote';
 
 console.log(randomQuotes());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-        main:path.resolve(__dirname, 'src/index.js')
+        bundle:path.resolve(__dirname, 'src/index.js')
     },
     output:{
         path: path.resolve(__dirname, 'dist'),
-        filename:'[name].js'
-    }
+        filename:'[name][contenthash].js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use:['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Webpack POC',
+            filename: 'index.html',
+            template:'src/template.html',
+            minify:true,
+            clean:true,
+        })
+    ]
 }
